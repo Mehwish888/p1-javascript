84319566672a5fad4da7dcde2d06cfa22813459a
Mehwish888 <90894368+Mehwish888@users.noreply.github.com>
2022-10-22 19:42:13 +0200

    opdracht 6.4 af

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,24 @@
+function calculate(num, num2) {
+    let answer = num + num2;
+    console.log(answer);
+}
+
+calculate(10, 12);
+
+
+
+function multiplyByFive(num) {
+    let answer = num * 5;
+    console.log(answer);
+}
+
+multiplyByFive(60);
+
+
+
+function calculateMinutes(num) {
+    let answer = num / 60;
+    console.log(answer);
+}
+
+calculateMinutes(5400);
