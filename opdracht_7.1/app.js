d4dc0de3d42b58f32e56dbd8db06c071eee53de9
Mehwish888 <90894368+Mehwish888@users.noreply.github.com>
2022-10-22 20:22:52 +0200

    opdracht 7.1 af

---
+++ opdracht_7.1/app.js
@@ -0,0 +1,11 @@
+let budget = 100;
+
+let product = 60;
+
+if (budget > product) {
+    title.innerText = "U heeft genoeg geld!";
+    title.style.color = "green";
+} else {
+    title.innerText = "Helaas, te weinig geld";
+    title.style.color = "red";
+}
