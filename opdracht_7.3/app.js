add22acaf1c0a26b9169c0206c2c9a2f219a18af
Mehwish888 <90894368+Mehwish888@users.noreply.github.com>
2022-10-24 21:08:37 +0200

    bezig

---
+++ opdracht_7.3/app.js
@@ -0,0 +1,9 @@
+let h1 = document.getElementById("number");
+
+function clicker() {
+    let count = h1.innerHTML;
+    ++count;
+    document.getElementById("number").innerHTML = count;
+}
+
+// NIET AF, BEGRIJP NIET
